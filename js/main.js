c7b90ac8ddb72f3b8ef4ac7b482d1dadbf9b919a
minjae970 <par15033@byui.edu>
2022-01-08 23:04:01 -0700

    er

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 
+const data = ["A", "B", "C"];
+
+let list = document.getElementById("linkList");
+
+data.forEach((item) => {
+  let temp = document.createElement("li");
+  temp.innerText = item;
+  list.appendChild(li);
+});
+
+
+
